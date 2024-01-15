3e2d74b6b9b7f96193f8acae51b6c2d63ef5757e
Tnixc <tnixxc@gmail.com>
2024-01-15 17:44:40 -0500

    Update code formatting and fix missing newlines at end of files

--- src/main/java/frc/robot/commands/DriveCommand.java
+++ src/main/java/frc/robot/commands/DriveCommand.java
@@ -3,6 +3,7 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.commands;
+
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import java.util.function.DoubleSupplier;
@@ -10,60 +11,63 @@ import java.util.function.DoubleSupplier;
 import frc.robot.subsystems.DrivebaseSubsystem;
 
 public class DriveCommand extends Command {
-    
-    private final DrivebaseSubsystem m_DrivebaseSubsystem;
-    private DoubleSupplier m_left_y;
-    private DoubleSupplier m_right_x;
 
-    public DriveCommand(DoubleSupplier left_y, DoubleSupplier right_x, DrivebaseSubsystem subsystem) {
-        m_left_y = left_y;
-        m_right_x = right_x;
-        m_DrivebaseSubsystem = subsystem;
-        addRequirements(m_DrivebaseSubsystem);
+  private final DrivebaseSubsystem m_DrivebaseSubsystem;
+  private DoubleSupplier m_left_y;
+  private DoubleSupplier m_right_x;
+
+  public DriveCommand(DoubleSupplier left_y, DoubleSupplier right_x, DrivebaseSubsystem subsystem) {
+    m_left_y = left_y;
+    m_right_x = right_x;
+    m_DrivebaseSubsystem = subsystem;
+    addRequirements(m_DrivebaseSubsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    double speed;
+    double rotation;
+
+    // TODO The comment below is incorrect. We need to test and see what direction
+    // does what.
+
+    // Up is negative on joystick. When we push the stick up, we want the robot to
+    // move in a positive direction, so we invert it here.
+    // Right is positive on the joystick. When we push the stick right, we want the
+    // robot to rotate clockwise (a negative direction), so we invert it here.
+    if (Math.abs(m_left_y.getAsDouble()) < 0.01) {
+      speed = 0;
+    } else {
+      speed = m_left_y.getAsDouble();
     }
 
-    // Called when the command is initially scheduled.
-    @Override
-    public void initialize() {
+    if (Math.abs(m_right_x.getAsDouble()) < 0.01) {
+      rotation = 0;
+    } else {
+      rotation = m_right_x.getAsDouble();
     }
 
-    // Called every time the scheduler runs while the command is scheduled.
-    @Override
-    public void execute() {
-        double speed;
-        double rotation;
+    m_DrivebaseSubsystem.set(speed, -rotation * 0.76);
 
-        // TODO The comment below is incorrect. We need to test and see what direction does what.
+    SmartDashboard.putNumber("Y", m_left_y.getAsDouble());
+    SmartDashboard.putNumber("X", m_right_x.getAsDouble());
+  }
 
-        // Up is negative on joystick. When we push the stick up, we want the robot to move in a positive direction, so we invert it here.
-        // Right is positive on the joystick. When we push the stick right, we want the robot to rotate clockwise (a negative direction), so we invert it here. 
-        if(Math.abs(m_left_y.getAsDouble()) < 0.01) {
-            speed = 0;
-        } else {
-            speed = m_left_y.getAsDouble();
-        }
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    m_DrivebaseSubsystem.stop();
+  }
 
-        if(Math.abs(m_right_x.getAsDouble()) < 0.01) {
-            rotation = 0;
-        } else {
-            rotation = m_right_x.getAsDouble();
-        }
-        
-        m_DrivebaseSubsystem.set(speed, -rotation*0.76);
-
-        SmartDashboard.putNumber("Y", m_left_y.getAsDouble());
-        SmartDashboard.putNumber("X", m_right_x.getAsDouble());
-    }
-
-    // Called once the command ends or is interrupted.
-    @Override
-    public void end(boolean interrupted) {
-        m_DrivebaseSubsystem.stop();
-    }
-
-    // Returns true when the command should end.
-    @Override
-    public boolean isFinished() {
-        return false;
-    }
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
 }
--- src/main/java/frc/robot/subsystems/DrivebaseSubsystem.java
+++ src/main/java/frc/robot/subsystems/DrivebaseSubsystem.java
@@ -11,71 +11,74 @@ import com.revrobotics.CANSparkLowLevel.MotorType;
 
 public class DrivebaseSubsystem extends SubsystemBase {
 
-    CANSparkMax m_leftDriveMotor1;
-    CANSparkMax m_leftDriveMotor2;
-    CANSparkMax m_rightDriveMotor1;
-    CANSparkMax m_rightDriveMotor2; 
+  CANSparkMax m_leftDriveMotor1;
+  CANSparkMax m_leftDriveMotor2;
+  CANSparkMax m_rightDriveMotor1;
+  CANSparkMax m_rightDriveMotor2;
 
-    private final DifferentialDrive m_drive;
+  private final DifferentialDrive m_drive;
 
-    private final Encoder m_leftDriveEncoder;
-    private final Encoder m_rightDriveEncoder;
+  private final Encoder m_leftDriveEncoder;
+  private final Encoder m_rightDriveEncoder;
 
-    private double m_scale = 1;
+  private double m_scale = 1;
 
-    public DrivebaseSubsystem () {
-        m_leftDriveMotor1 = new CANSparkMax(Constants.CAN.LEFT_DRIVE_MOTOR_1, MotorType.kBrushless);
-        m_leftDriveMotor1.restoreFactoryDefaults();
-        m_leftDriveMotor1.setSmartCurrentLimit(38);
+  public DrivebaseSubsystem() {
+    m_leftDriveMotor1 = new CANSparkMax(Constants.CAN.LEFT_DRIVE_MOTOR_1, MotorType.kBrushless);
+    m_leftDriveMotor1.restoreFactoryDefaults();
+    m_leftDriveMotor1.setSmartCurrentLimit(38);
 
-        m_leftDriveMotor2 = new CANSparkMax(Constants.CAN.LEFT_DRIVE_MOTOR_2, MotorType.kBrushless);
-        m_leftDriveMotor2.restoreFactoryDefaults();
-        m_leftDriveMotor2.setSmartCurrentLimit(38);
+    m_leftDriveMotor2 = new CANSparkMax(Constants.CAN.LEFT_DRIVE_MOTOR_2, MotorType.kBrushless);
+    m_leftDriveMotor2.restoreFactoryDefaults();
+    m_leftDriveMotor2.setSmartCurrentLimit(38);
 
-        m_rightDriveMotor1 = new CANSparkMax(Constants.CAN.RIGHT_DRIVE_MOTOR_1, MotorType.kBrushless);
-        m_rightDriveMotor1.restoreFactoryDefaults();
-        m_rightDriveMotor1.setSmartCurrentLimit(38);
+    m_rightDriveMotor1 = new CANSparkMax(Constants.CAN.RIGHT_DRIVE_MOTOR_1, MotorType.kBrushless);
+    m_rightDriveMotor1.restoreFactoryDefaults();
+    m_rightDriveMotor1.setSmartCurrentLimit(38);
 
-        m_rightDriveMotor2 = new CANSparkMax(Constants.CAN.RIGHT_DRIVE_MOTOR_2, MotorType.kBrushless);
-        m_rightDriveMotor2.restoreFactoryDefaults();
-        m_rightDriveMotor2.setSmartCurrentLimit(38);
+    m_rightDriveMotor2 = new CANSparkMax(Constants.CAN.RIGHT_DRIVE_MOTOR_2, MotorType.kBrushless);
+    m_rightDriveMotor2.restoreFactoryDefaults();
+    m_rightDriveMotor2.setSmartCurrentLimit(38);
 
-        m_leftDriveMotor2.follow(m_leftDriveMotor1);
-        m_leftDriveMotor1.setInverted(Constants.Drive.LEFT_DRIVE_INVERTED);
-        m_rightDriveMotor2.follow(m_rightDriveMotor2);
-        m_rightDriveMotor1.setInverted(Constants.Drive.RIGHT_DRIVE_INVERTED);
+    m_leftDriveMotor2.follow(m_leftDriveMotor1);
+    m_leftDriveMotor1.setInverted(Constants.Drive.LEFT_DRIVE_INVERTED);
+    m_rightDriveMotor2.follow(m_rightDriveMotor2);
+    m_rightDriveMotor1.setInverted(Constants.Drive.RIGHT_DRIVE_INVERTED);
 
-        m_drive = new DifferentialDrive(m_leftDriveMotor1, m_rightDriveMotor1);
+    m_drive = new DifferentialDrive(m_leftDriveMotor1, m_rightDriveMotor1);
 
-        setDrivebaseIdle(IdleMode.kBrake);
-        m_leftDriveEncoder = new Encoder(Constants.Digital.LEFT_DRIVE_ENCODER[0], Constants.Digital.LEFT_DRIVE_ENCODER[1]);
-        m_rightDriveEncoder = new Encoder(Constants.Digital.RIGHT_DRIVE_ENCODER[0], Constants.Digital.RIGHT_DRIVE_ENCODER[1]);
-        // when robot goes forward, left encoder spins positive and right encoder spins negative
-        m_leftDriveEncoder.setDistancePerPulse(Constants.Drive.DISTANCE_PER_ENCODER_PULSE);
-        m_rightDriveEncoder.setDistancePerPulse(Constants.Drive.DISTANCE_PER_ENCODER_PULSE);
-        m_leftDriveEncoder.setReverseDirection(Constants.Drive.LEFT_DRIVE_INVERTED);
-        m_rightDriveEncoder.setReverseDirection(Constants.Drive.RIGHT_DRIVE_INVERTED);
-        m_leftDriveEncoder.reset();
-        m_rightDriveEncoder.reset();
+    setDrivebaseIdle(IdleMode.kBrake);
+    m_leftDriveEncoder = new Encoder(Constants.Digital.LEFT_DRIVE_ENCODER[0],
+        Constants.Digital.LEFT_DRIVE_ENCODER[1]);
+    m_rightDriveEncoder = new Encoder(Constants.Digital.RIGHT_DRIVE_ENCODER[0],
+        Constants.Digital.RIGHT_DRIVE_ENCODER[1]);
+    // when robot goes forward, left encoder spins positive and right encoder spins
+    // negative
+    m_leftDriveEncoder.setDistancePerPulse(Constants.Drive.DISTANCE_PER_ENCODER_PULSE);
+    m_rightDriveEncoder.setDistancePerPulse(Constants.Drive.DISTANCE_PER_ENCODER_PULSE);
+    m_leftDriveEncoder.setReverseDirection(Constants.Drive.LEFT_DRIVE_INVERTED);
+    m_rightDriveEncoder.setReverseDirection(Constants.Drive.RIGHT_DRIVE_INVERTED);
+    m_leftDriveEncoder.reset();
+    m_rightDriveEncoder.reset();
 
-    }
-    
-    public void setDrivebaseIdle(IdleMode setting) {
-        m_rightDriveMotor1.setIdleMode(setting);
-        m_rightDriveMotor2.setIdleMode(setting);
-        m_leftDriveMotor1.setIdleMode(setting);
-        m_leftDriveMotor2.setIdleMode(setting);
-    }
+  }
 
-    public void stop(){
-        set(0,0);
-    }
+  public void setDrivebaseIdle(IdleMode setting) {
+    m_rightDriveMotor1.setIdleMode(setting);
+    m_rightDriveMotor2.setIdleMode(setting);
+    m_leftDriveMotor1.setIdleMode(setting);
+    m_leftDriveMotor2.setIdleMode(setting);
+  }
 
-    public void set(double speed, double rotation) {
-        m_drive.curvatureDrive(speed*m_scale, rotation*m_scale, true);
-    }
+  public void stop() {
+    set(0, 0);
+  }
 
-    public void setScale(double scale) {
-        m_scale = scale;
-    }
+  public void set(double speed, double rotation) {
+    m_drive.curvatureDrive(speed * m_scale, rotation * m_scale, true);
+  }
+
+  public void setScale(double scale) {
+    m_scale = scale;
+  }
 }
