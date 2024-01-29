f3a1d37d523dc77bac76aa9473f5a2286401f1ec
tqbed <66844373+tqbed@users.noreply.github.com>
2024-01-29 18:53:36 -0500

    Minor spelling mistake (I lose)

--- src/main/java/frc/robot/subsystems/MotorSubsystem.java
+++ src/main/java/frc/robot/subsystems/MotorSubsystem.java
@@ -2,7 +2,6 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import com.ctre.phoenix6.hardware.TalonFX;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
@@ -28,8 +27,7 @@ public void setTalonSpeed(double speed) {
 }
 
 public void setSparkSpeed(double speed) {
-  // Set the motor speed using the Talon SRX
+  // Set the motor speed using the Spark MAX
   m_spark.set(speed); 
 }
-
 }
