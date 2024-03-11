9ca23f67af9448bd269226edf9dcc78a184755b6
tqbed <66844373+tqbed@users.noreply.github.com>
2024-03-11 16:15:41 -0400

    less ramp

--- src/main/java/frc/robot/commands/ShootSequenceFull.java
+++ src/main/java/frc/robot/commands/ShootSequenceFull.java
@@ -23,7 +23,7 @@ public class ShootSequenceFull extends SequentialCommandGroup {
 
     this.addCommands(new InstantCommand(() -> m_led.setMode(Constants.LED.modes.Yellow)));
     this.addCommands(new InstantCommand(() -> m_shooter.setFlywheels(1)));
-    this.addCommands(new WaitCommand(0.8));
+    this.addCommands(new WaitCommand(0.5));
     this.addCommands(new InstantCommand(() -> m_intake.setBelt(1)));
     this.addCommands(new WaitCommand(1));
 
