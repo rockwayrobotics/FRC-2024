22b77232618c8f3779fc4552df4f421fcaa17d4f
tqbed <66844373+tqbed@users.noreply.github.com>
2024-03-12 19:08:21 -0400

    soft limit testing stuff added at the field

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -1,7 +1,9 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkBase.SoftLimitDirection;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.networktables.GenericEntry;
@@ -14,12 +16,17 @@ import frc.robot.Constants;
 
 public class ClimberSubsystem extends SubsystemBase {
   private final CANSparkMax m_climberMotor;
+  private final RelativeEncoder m_climberEncoder;
 
   private DigitalInput m_homeSensor;
 
+
   ShuffleboardTab dashboardTab = Shuffleboard.getTab("NewDashboard");
 
-  GenericEntry homesensor;
+  GenericEntry homeSensorEntry;
+  GenericEntry climberEncoderEntry;
+  GenericEntry climberSpeedEntry; 
+  GenericEntry softLimitEntry;
 
   /** Creates a new HookSubsystem. */
   public ClimberSubsystem(int climberMotor /* , int topLimitSwitch, int bottomLimitSwitch */) {
@@ -27,11 +34,31 @@ public class ClimberSubsystem extends SubsystemBase {
     m_climberMotor.setIdleMode(IdleMode.kBrake);
     m_climberMotor.setInverted(true);
 
+    m_climberMotor.setSoftLimit(SoftLimitDirection.kForward, 150);
+    m_climberMotor.setSoftLimit(SoftLimitDirection.kReverse, 50);
+    m_climberMotor.enableSoftLimit(SoftLimitDirection.kReverse, true);
+    m_climberMotor.enableSoftLimit(SoftLimitDirection.kForward, true);
+
+    m_climberEncoder = m_climberMotor.getEncoder();
+
     m_homeSensor = new DigitalInput(Constants.Digital.CLIMB_HOME_SWITCH);
 
-    homesensor = 
+    homeSensorEntry = 
     dashboardTab.addPersistent("Climb Home Sensor", false)
     .getEntry();
+
+    climberEncoderEntry =
+    dashboardTab.add("Climber Encoder", 0)
+    .getEntry();
+
+    climberSpeedEntry = 
+    dashboardTab.add("Climber Speed", 0)
+    .getEntry();
+
+    softLimitEntry =
+    dashboardTab.add("Climber Soft Limit", false)
+    .getEntry();
+
   }
 
   public boolean isAtHome(){
@@ -61,8 +88,13 @@ public class ClimberSubsystem extends SubsystemBase {
         m_climberMotor.set(0);
       }
     }
-    homesensor.setBoolean(isAtHome());
+    homeSensorEntry.setBoolean(isAtHome());
+    climberEncoderEntry.setDouble(m_climberEncoder.getPosition());
+    climberSpeedEntry.setDouble(m_climberMotor.get());
+    boolean softLimitEnable = softLimitEntry.getBoolean(false);
+      m_climberMotor.enableSoftLimit(SoftLimitDirection.kReverse, softLimitEnable);
+      m_climberMotor.enableSoftLimit(SoftLimitDirection.kForward, softLimitEnable);
+    }
   }
-}
 
 
