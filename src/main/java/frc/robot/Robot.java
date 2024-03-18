f690169f2d9f83a660c867ec077c3dcec20feb32
tqbed <66844373+tqbed@users.noreply.github.com>
2024-03-18 17:39:28 -0400

    reset angle setpoint on robot entering disabled mode

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -62,6 +62,7 @@ public class Robot extends TimedRobot {
   /** This function is called once each time the robot enters Disabled mode. */
   @Override
   public void disabledInit() {
+    m_robotContainer.m_angler.angleSetpointWidget.setDouble(0);
   }
 
   @Override
